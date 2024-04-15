c7e250b9e3e380287a243445c8eeed21dc5b2f40
sh4peee <vadim.kalashnikov.9595@mail.ru>
2024-04-15 21:26:46 +0300

    Update 1

--- src/main/java/kalashnikov/v/s/aimcoach/AimCoachApp.java
+++ src/main/java/kalashnikov/v/s/aimcoach/AimCoachApp.java
@@ -18,30 +18,26 @@ import java.util.List;
 import java.util.Random;
 
 public class AimCoachApp extends Application {
-
     public static final int LENGTH = 600;
     public static final int WIDTH = 800;
-
     public static final int TARGET_RADIUS_EASY = 25;
     public static final int TARGET_RADIUS_MEDIUM = 20;
     public static final int TARGET_RADIUS_HARD = 15;
     private int targetRadius = TARGET_RADIUS_EASY;
-
     public static final int TARGET_COUNT = 3;
     private int score = 0;
     private int record = 0;
     private List<Target> targets = new ArrayList<>();
     private AnimationTimer timer;
     private long lastTime;
-    private long targetTime = 10 * 1_000_000_000L; // 10 секунд в наносекундах
-
+    private long targetTime = 20 * 1_000_000_000L; // 20 секунд в наносекундах
     private Label scoreLabel;
     private Label recordLabel;
     private Label timeLabel;
-
     private Stage primaryStage;
     private Scene menuScene;
     private Scene gameScene;
+    private ComboBox<String> soundComboBox;
     private boolean isGamePaused = false;
 
     @Override
@@ -70,8 +66,33 @@ public class AimCoachApp extends Application {
         // Создаем сцену игры и присваиваем ее gameScene
         gameScene = createGameScene();
     }
-
     private Scene createMenuScene() {
+        // Выпадающий список для выбора звука попадания
+        soundComboBox = new ComboBox<>();
+        soundComboBox.getItems().addAll("ak47.wav", "awp.wav", "deagle.wav");
+        soundComboBox.setValue("ak47.wav"); // Устанавливаем значение по умолчанию
+
+        // Создаем выпадающий список для выбора времени игры
+        ComboBox<String> timeComboBox = new ComboBox<>();
+        timeComboBox.getItems().addAll("20 sec", "40 sec", "60 sec");
+        timeComboBox.setValue("20 sec"); // Устанавливаем значение по умолчанию
+
+        // Обработка выбора времени игры
+        timeComboBox.setOnAction(event -> {
+            String selectedTime = timeComboBox.getValue();
+            switch (selectedTime) {
+                case "20 sec":
+                    targetTime = 20 * 1_000_000_000L;
+                    break;
+                case "40 sec":
+                    targetTime = 40 * 1_000_000_000L;
+                    break;
+                case "60 sec":
+                    targetTime = 60 * 1_000_000_000L;
+                    break;
+            }
+        });
+
         // Создаем кнопку для входа в игру
         Button startGameButton = new Button("Start Game");
         startGameButton.setOnAction(event -> {
@@ -82,6 +103,7 @@ public class AimCoachApp extends Application {
             generateTargets();
             lastTime = System.nanoTime();
             startTimer();
+
         });
 
         // Создаем кнопку для выхода из приложения
@@ -98,7 +120,7 @@ public class AimCoachApp extends Application {
         Button changeDifficultyButton = new Button("Change Difficulty");
         changeDifficultyButton.setOnAction(event -> {
             // Изменяем сложность игры и обновляем метку
-            if (targetRadius == TARGET_RADIUS_EASY) {
+            if (targetRadius== TARGET_RADIUS_EASY) {
                 targetRadius = TARGET_RADIUS_MEDIUM;
                 difficultyLabel.setText("Difficulty: Medium");
             } else if (targetRadius == TARGET_RADIUS_MEDIUM) {
@@ -121,20 +143,22 @@ public class AimCoachApp extends Application {
         // Создаем вертикальную панель для расположения элементов меню
         VBox menuLayout = new VBox(20);
         menuLayout.setAlignment(Pos.CENTER);
+        menuLayout.getChildren().addAll(timeComboBox);
         menuLayout.getChildren().addAll(startGameButton, changeDifficultyButton, exitButton, difficultyLabel, scoreLabel, recordLabel);
+        menuLayout.getChildren().add(soundComboBox);
 
         // Создаем главную панель
         BorderPane menuPane = new BorderPane();
         menuPane.setCenter(menuLayout);
+        menuPane.setTop(timeComboBox);
 
         // Создаем сцену меню
         Scene menuScene = new Scene(menuPane, WIDTH, LENGTH);
 
         return menuScene;
     }
-
-
     private Scene createGameScene() {
+
         // Создаем полотно для рисования
         Canvas gameCanvas = new Canvas(WIDTH, LENGTH);
 
@@ -142,7 +166,7 @@ public class AimCoachApp extends Application {
         GraphicsContext gc = gameCanvas.getGraphicsContext2D();
 
         // Создаем метку для отображения оставшегося времени
-        timeLabel = new Label("Time Left: " + (targetTime / 1_000_000_000)); // Переводим наносекунды в секунды
+        timeLabel = new Label("Time Left: " + (targetTime / 1_000_000_000)); // Переводим наносекунды всекунды
 
         // Создаем вертикальную панель для расположения элементов игры
         VBox gameLayout = new VBox(20);
@@ -157,7 +181,6 @@ public class AimCoachApp extends Application {
         // Создаем сцену игры
         Scene gameScene = new Scene(gamePane, WIDTH, LENGTH);
 
-
         // Обработка нажатия на полотно игры
         gameCanvas.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
             Iterator<Target> iterator = targets.iterator();
@@ -167,6 +190,17 @@ public class AimCoachApp extends Application {
                     score++; // Увеличиваем счет
                     scoreLabel.setText("Score: " + score); // Обновляем метку счета
                     target.pop(); // Помечаем текущий шарик как "взорванный"
+
+                    // Получаем выбранный звук из ComboBox
+                    String selectedSound = soundComboBox.getValue();
+                    // Создаем путь к файлу звука попадания
+                    String soundPath = "src/Sounds/" + selectedSound;
+                    // запускает звук с заданной громкостью( от 0 до 1)_
+                    SoundPlayer.playSound(soundPath).setVolume((float) 0.4);
+                    // запускает звук и остановит все остальные потоки
+                    //SoundPlayer.playSound("src/Sounds/ak47.wav").join();
+                    // запускает звук не останавливая потоки и смешивая звуки
+                    //SoundPlayer.playSound("sounds/hello.wav");
 
                     // Создаем новый шарик и добавляем его в список целей
                     double x = new Random().nextDouble() * (WIDTH - targetRadius * 2) + targetRadius;
@@ -181,12 +215,10 @@ public class AimCoachApp extends Application {
                 }
             }
         });
-
         targets.clear();
         generateTargets();
         // Рисуем шарики на полотне
         drawTargets(gc);
-
         return gameScene;
     }
 
@@ -196,7 +228,6 @@ public class AimCoachApp extends Application {
             target.draw(gc); // Рисуем цели
         }
     }
-
 
     private void startTimer() {
         timer = new AnimationTimer() {
@@ -224,31 +255,19 @@ public class AimCoachApp extends Application {
         };
         timer.start();
     }
-
     private void resetScore() {
         score = 0;
         scoreLabel.setText("Score: " + score);
     }
-
     private void generateTargets() {
-        //targets.clear();
         Random random = new Random();
         int remainingTargets = TARGET_COUNT - targets.size();
         for (int i = 0; i < remainingTargets; i++) {
-            double x = random.nextDouble() * (WIDTH - targetRadius * 2) + targetRadius;
-            double y = random.nextDouble() * (LENGTH - targetRadius * 2) + targetRadius;
+            double x = random.nextDouble() * (WIDTH - targetRadius * 2) + targetRadius - targets.size();
+            double y = random.nextDouble() * (LENGTH - targetRadius * 2) + targetRadius - targets.size();
             targets.add(new Target(x, y, targetRadius));
         }
-
     }
-
-    private Target generateTarget() {
-        Random random = new Random();
-        double x = random.nextDouble() * (WIDTH - targetRadius * 2) + targetRadius;
-        double y = random.nextDouble() * (LENGTH - targetRadius * 2) + targetRadius;
-        return new Target(x, y, targetRadius);
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.aimcoach {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jlayer;
+    requires java.desktop;
 
 
     opens kalashnikov.v.s.aimcoach to javafx.fxml;
